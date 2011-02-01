ec3ae0f7b1989867ff655832d64ee41a2008410a
cschwarz <cschwarz@fa00571f-6d07-0410-8660-cfe97aa2585a>
2011-02-01 07:05:21 +0000

    * Small gpp branch merging cleanups (/dev/humancontroller/)  - Merge r1933-r1935 that were accidentally forgotten for all this time  - Delete a few files that are no longer used and no longer in trunk
    git-svn-id: svn://svn.icculus.org/tremulous/branches/gpp@2143 fa00571f-6d07-0410-8660-cfe97aa2585a

--- src/game/g_local.h
+++ src/game/g_local.h
@@ -1087,7 +1087,6 @@ extern  vmCvar_t  g_maxclients;     // allow this many total, including spectato
 extern  vmCvar_t  g_maxGameClients;   // allow this many active
 extern  vmCvar_t  g_restarted;
 extern  vmCvar_t  g_lockTeamsAtStart;
-extern  vmCvar_t  g_minCommandPeriod;
 extern  vmCvar_t  g_minNameChangePeriod;
 extern  vmCvar_t  g_maxNameChanges;
 
@@ -1117,9 +1116,6 @@ extern  vmCvar_t  g_teamForceBalance;
 extern  vmCvar_t  g_smoothClients;
 extern  vmCvar_t  pmove_fixed;
 extern  vmCvar_t  pmove_msec;
-extern  vmCvar_t  g_enableDust;
-extern  vmCvar_t  g_enableBreath;
-extern  vmCvar_t  g_singlePlayer;
 
 extern  vmCvar_t  g_alienBuildPoints;
 extern  vmCvar_t  g_alienBuildQueueTime;
